e16dedef14e9b65475d25ca45a1bb30d9f24f0f7
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-24 20:31:37 +0300

    Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -13,7 +13,9 @@ namespace TypesAndVariables
             //Values Types
             // Console.WriteLine("Hello World!");
             int number1 = 2147483647;
+            long number2= 9223372036854775807;
             Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number2 is {0}", number2);
             Console.ReadLine();
         }
     }

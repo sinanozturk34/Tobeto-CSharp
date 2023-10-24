628d07b18bc5d4c94dedc629c0fd88d828f38233
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-24 19:53:44 +0300

    TypesAndVariables

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -14,7 +14,6 @@ namespace TypesAndVariables
             // Console.WriteLine("Hello World!");
             int number1 = 2147483647;
             Console.WriteLine("Number1 is {0}",number1);
-
             Console.ReadLine();
         }
     }

2d5510ce0e188020f404f671e638ca1895290d40
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-24 19:45:20 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Values Types
+            // Console.WriteLine("Hello World!");
+            int number1 = 2147483647;
+            Console.WriteLine("Number1 is {0}",number1);
+
+            Console.ReadLine();
+        }
+    }
+}

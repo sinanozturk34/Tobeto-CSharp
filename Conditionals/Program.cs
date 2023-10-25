e34504439eadc79349684c3f6fe0546ca0d99289
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 11:14:51 +0300

    else if bloklarıyla çalışma

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -11,17 +11,21 @@ namespace Conditionals
         static void Main(string[] args)
         {
             var number = 11;
-            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
-            /*
+           // Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+            
             if (number == 10)
             {
                 Console.WriteLine("Number is 10");
             }
+            else if(number == 20)
+            {
+                Console.WriteLine("Number is 20");
+            }
             else
             {
-                Console.WriteLine("Number is not 10");
+                Console.WriteLine("Number is not 10 or 20");
             }
-            */
+            
             Console.ReadLine();//çıktı okuması için
         }
     }

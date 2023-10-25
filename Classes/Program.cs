a45ebd7fc40f40979e4a15c2a3c8436a11dbbaf5
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 14:10:13 +0300

    Classlara Giriş

---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Console.ReadLine();
+        }
+    }
+   
+   
+}

03015ee9924dffb1e001a56b89a9d5872f353e6c
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 08:46:42 +0300

    Short Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,10 +12,16 @@ namespace TypesAndVariables
         {
             //Values Types
             // Console.WriteLine("Hello World!");
+            //integer veri tipi
+            short number3 = 32767;
+
             int number1 = 2147483647;
             long number2= 9223372036854775807;
             Console.WriteLine("Number1 is {0}",number1);
+            //long veri tipi
             Console.WriteLine("Number2 is {0}", number2);
+            //short veri tipi
+            Console.WriteLine("Number3 is {0}", number3);
             Console.ReadLine();
         }
     }

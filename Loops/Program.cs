cb8af6e73dae717b3a834d7f8ea7555e179e2c63
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 13:25:10 +0300

    foreach döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -20,13 +20,21 @@ namespace Loops
             }
             Console.WriteLine("Now number is {0}", number);
             */
-
+            /*
             int number = 10;
             do
             {
                 Console.WriteLine(number);
                 number--;
             } while (number >= 0);
+            */
+
+            string[] students = new string[3] {"Engin","Derin","Salih"};
+            foreach(var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
             Console.ReadLine();
         }
         private static void ForLoop()

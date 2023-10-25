ee403a8fa00a41905df0bd3f88f1a1054c581b2a
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 13:16:59 +0300

    while dongusu

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,14 +10,24 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            for(int i=100; i >= 0; i=i-2) //tek sayıları yazma i=1 yada i=2 göre i=i+2
+            //ForLoop();
+            int number = 100;
+            while (number>=0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+            Console.ReadLine();
+        }
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2) 
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine("Finished!");
 
-
-            Console.ReadLine();
         }
     }
 }

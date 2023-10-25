8fdba935c8c1370a1e29bdce84cb185bc236f841
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 13:38:05 +0300

    Asal Sayı

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -20,6 +20,7 @@ namespace Loops
             }
             Console.WriteLine("Now number is {0}", number);
             */
+
             /*
             int number = 10;
             do
@@ -29,14 +30,46 @@ namespace Loops
             } while (number >= 0);
             */
 
+            /*
             string[] students = new string[3] {"Engin","Derin","Salih"};
             foreach(var student in students)
             {
                 Console.WriteLine(student);
             }
+            */
+            //---------------------------------
+            //Asal Sayı
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number ");
+            }
+            else
+            {
+                Console.WriteLine("This is not a prime number");
+            }
 
             Console.ReadLine();
         }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+
+
+
+
+
+
         private static void ForLoop()
         {
             for (int i = 100; i >= 0; i = i - 2) 

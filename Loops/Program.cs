20e7345e76421c870254445fde82bcf2985770aa
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 13:10:07 +0300

    For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=100; i >= 0; i=i-2) //tek sayıları yazma i=1 yada i=2 göre i=i+2
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+
+
+            Console.ReadLine();
+        }
+    }
+}

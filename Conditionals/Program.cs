7a843fe7c925565fced6153a095436e9037bc51d
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 11:23:38 +0300

    switch bloguyla çalışma

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -11,8 +11,8 @@ namespace Conditionals
         static void Main(string[] args)
         {
             var number = 11;
-           // Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
-            
+            // Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+            /*
             if (number == 10)
             {
                 Console.WriteLine("Number is 10");
@@ -25,7 +25,19 @@ namespace Conditionals
             {
                 Console.WriteLine("Number is not 10 or 20");
             }
-            
+            */
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
+            }
             Console.ReadLine();//çıktı okuması için
         }
     }

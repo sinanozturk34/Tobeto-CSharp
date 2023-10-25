5d584dcd520315085286e0983300c326669fe735
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 11:43:34 +0300

    Neden metot kullanıyoruz

---
+++ Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();//metot çagırdık
+            Add();
+            Add();
+            Console.ReadLine();
+        }
+        static void Add() //metot
+        {
+            Console.WriteLine("Added!!!");
+        }
+    }
+}

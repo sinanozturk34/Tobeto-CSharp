3eb37ecac3b4d45dac5b194940bf78acc40374cb
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 08:55:15 +0300

    Byte Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,16 +12,18 @@ namespace TypesAndVariables
         {
             //Values Types
             // Console.WriteLine("Hello World!");
-            //integer veri tipi
+            byte number4 = 255;
             short number3 = 32767;
-
             int number1 = 2147483647;
             long number2= 9223372036854775807;
+            //integer veri tipi
             Console.WriteLine("Number1 is {0}",number1);
             //long veri tipi
             Console.WriteLine("Number2 is {0}", number2);
             //short veri tipi
             Console.WriteLine("Number3 is {0}", number3);
+            //byte 
+            Console.WriteLine("Number4 is {0}", number4);
             Console.ReadLine();
         }
     }

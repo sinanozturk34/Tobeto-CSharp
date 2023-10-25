4a83178fae0128838bebccf74e47ee467800baee
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-25 13:00:38 +0300

    Çok Boyutlu Diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -17,13 +17,34 @@ namespace Arrays
             students[2] = "Salih"; */
 
             //Array
-            string[] students2 = {"Engin","Derin","Salih"};
-           
+            string[] students2 = { "Engin", "Derin", "Salih" };
 
+            /*
             foreach (var student in students2)
             {
                 Console.WriteLine(student);
             }
+            */
+            string[,] regions = new string[5, 3]
+           {
+               { "İstanbul" , "İzmit" , "Balıkesir"},
+               { "Ankara" , "Konya" , "Kırıkkale" },
+               { "Antalya" , "Adana" , "Mersin" },
+               { "Rize" , "Trabzon" , "Samsun" },
+               { "İzmir" , "Muğla" , "Manisa"},
+           };
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+               
+            }
+
+
+
+
             Console.WriteLine();
             Console.ReadLine();
         }

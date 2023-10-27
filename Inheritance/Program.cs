d934aa35a6cf92cf9c594f0bdb86e8117c876949
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-27 16:03:57 +0300

    Kalıtıma Giriş

---
+++ Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] persons = new Person[2]
+            {
+                new Customer {FirstName = "Merve"}, new Student{FirstName = "Defne"},
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+                Console.ReadLine();
+            }
+
+        }
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        }
+
+        class Customer : Person
+        {
+            public string City { get; set; }
+        }
+        class Student : Person
+        {
+            public string Department { get; set; }
+        }
+    }
+    }
+}

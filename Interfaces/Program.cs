92e5a4fecafc2035025c44f93549a8f3321605d9
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-27 15:20:43 +0300

    İnterfacelere Neden Kullanıyoruz?

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,33 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+
+
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Computer Sciences"
+            };
+            manager.Add(student);
+            manager.Add(customer);
+
+           // manager.Add(customer);
+
+            //manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" });
+
+            Console.ReadLine();
         }
     }
 
@@ -33,6 +61,24 @@ namespace Interfaces
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 
 }

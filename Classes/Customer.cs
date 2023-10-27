aa7779cb47007d20d6940bbfef5decc5fece51d9
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-10-27 13:06:49 +0300

    Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -5,12 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Classes
-{
+{//encapsulation
     class Customer
     {
-        public string FirstName;
+        //field : alan(degişken) tanımlamak
+       // public string FirstName;
+        
+        //Property:özellik tanımlamak
         public int Id { get; set; }
-        //public string FirstName { get; set; }
+        public string FirstName { get; set; }
+        /*   //encapsulation
+        private string _firstName;
+        public string FirstName 
+        {
+            get { return "Mrs." + _firstName; }
+            set { _firstName = value; }
+        }
+        */
         public string LastName { get; set; }
         public string City { get; set; }
 
